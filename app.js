b2f1d04e52379a3b21b1b99451eaa3f9022f8f25
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-01-03 13:39:05 -0700

    commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const data = require('./data')
+const moves = require('./data/moves')
 const app = express()
 const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.json(data))
+app.get('/', (req, res) => res.json(moves))
 
 app.listen(process.env.PORT || 3000, () => console.log('listening'))
