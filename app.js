045f22845b40fe42b5ac8e2a997579f1e382906f
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-01-03 17:53:42 -0700

    commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const moves = require('./data/moves')
+const chars = require('./data/chars')
 const app = express()
 const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.json(moves))
+app.get('/', (req, res) => res.json(chars))
 
 app.listen(process.env.PORT || 3000, () => console.log('listening'))
---
+++ data/chars.js
@@ -0,0 +1,41 @@
+
+
+var moves = [{
+      "name": "Ultron",
+      "move1": "Melt Blast - stuff"
+    },
+    {
+      "name": "Nova",
+      "move1": "Move - Move description"
+    },
+    {
+      "name": "Dormammu",
+      "move1": "Move"
+    },
+    {
+      "name": "Dr. Strange",
+      "move1": "Move"
+    },
+    {
+      "name": "Ghost Rider",
+      "move1": "Move"
+    },
+    {
+    "name": "Hulk",
+    "move1": "Move"
+    }
+]
+
+var combos = [{
+    "name": "Ultron",
+    "combo1": "Midscreen BnB - stuff"
+  },
+  {
+    "name": "Nova",
+    "combo1": "stuff - stuff"
+}]
+
+module.exports = {
+  moves,
+  combos
+}
