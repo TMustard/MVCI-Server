4198e54f692b3d811eeb1c6f649a0c60d16a48d5
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-01-12 11:10:35 -0700

    commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const chars = require('./data/chars')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const chars = require("./data/chars");
+const app = express();
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json ());
 
-app.get('/', (req, res) => res.json(chars))
+app.get("/", (req, res) => res.json(chars));
 
-app.listen(process.env.PORT || 3000, () => console.log('listening'))
+app.listen(process.env.PORT || 3000, () => console.log("listening"));
--- data/chars.js
+++ data/chars.js
@@ -1,41 +1,69 @@
-
-
-var moves = [{
-      name: "Ultron",
-      move1: "Melt Blast - stuff"
-    },
-    {
-      "name": "Nova",
-      "move1": "Move - Move description"
-    },
-    {
-      "name": "Dormammu",
-      "move1": "Move"
-    },
-    {
-      "name": "Dr. Strange",
-      "move1": "Move"
-    },
-    {
-      "name": "Ghost Rider",
-      "move1": "Move"
-    },
-    {
-    "name": "Hulk",
-    "move1": "Move"
-    }
-]
-
-var combos = [{
-    "name": "Ultron",
-    "combo1": "Midscreen BnB - stuff"
+var moves = [
+  {
+    name: "Ultron",
+    moves: ["yo                    ", "sup"]
   },
   {
-    "name": "Nova",
-    "combo1": "stuff - stuff"
-}]
+    name: "Nova",
+    moves: [
+      "Aerial Uppercut- (in air) Forward + LP",
+      "Backstep Kick- (on hit) HK, HK",
+      "Moon Tracer Kick- Forward + HK",
+      "Nova Slam- Forward + HP",
+      "Shooting Punch- Down + Forward + LP",
+      "Energy Javelin- Down, Down, P",
+      "Centurion Rush- (air ok) QCB, K",
+      "Flight- (air ok) QCB, KK",
+      "Gravimetric Blaster (1 bar)- (air ok) QCF, PP",
+      "Super Nova (1 bar)- (air ok) QCB, PP",
+      "Level 3 Hyper (3 bars)- QCF, KK"
+    ]
+  },
+  {
+    name: "Dormammu",
+    moves: "Move"
+  },
+  {
+    name: "Dr. Strange",
+    moves: "Move"
+  },
+  {
+    name: "Ghost Rider",
+    moves: "Move"
+  },
+  {
+    name: "Hulk",
+    moves: "Move"
+  }
+];
+
+var combos = [
+  {
+    name: "Ultron",
+    combos: ["Midscreen BnB - stuff", "Combo - stuff"]
+  },
+  {
+    name: "Nova",
+    combos: ["combo - stuff", "otherCombo - Stuff"]
+  }
+];
 
 module.exports = {
   moves,
   combos
-}
+};
+
+// var nova = {
+//   name:   'Nova',
+// move1:  'Aerial Uppercut- (in air) Forward + LP',
+// move2:  'Backstep Kick- (on hit) HK, HK',
+// move3:  'Moon Tracer Kick- Forward + HK',
+// move4:  'Nova Slam- Forward + HP',
+// move5:  'Shooting Punch- Down + Forward + LP',
+// move6:  'Energy Javelin- Down, Down, P',
+// move7:  'Centurion Rush- (air ok) QCB, K',
+// move8:  'Flight- (air ok) QCB, KK',
+// move9:  'Gravimetric Blaster (1 bar)- (air ok) QCF, PP',
+// move10: 'Super Nova (1 bar)- (air ok) QCB, PP',
+// move11: 'Level 3 Hyper (3 bars)- QCF, KK'
+// }
